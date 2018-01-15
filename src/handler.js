e48420371ff2ffb043f119de6c249a941262cc4a
Seth Orell <seth.orell@gmail.com>
2018-01-15 14:10:34 -0600

    Rename service and path

--- src/handler.js
+++ src/handler.js
@@ -2,7 +2,7 @@ module.exports.helloWorld = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.25! Your function executed successfully!',
+      message: 'Go Serverless v1.25! Your ALPHA function executed successfully!',
       input: event,
     }),
   };
